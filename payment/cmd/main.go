fe708181d417c460dc1a5d7c05f31adf4f9b528d
kirillmc <kirillmcdev@gmail.com>
2025-07-13 19:25:17 +0300

    feat [ week_01 ] Реализован payment сервис

---
+++ payment/cmd/main.go
@@ -0,0 +1,69 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"github.com/google/uuid"
+	paymentV1 "github.com/kirillmc/starShipsCompany/shared/pkg/proto/payment/v1"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/reflection"
+	"log"
+	"net"
+	"os"
+	"os/signal"
+	"syscall"
+)
+
+const grpcPort = 50052
+
+func main() {
+	lis, err := net.Listen("tcp", fmt.Sprintf(":%d", grpcPort))
+	if err != nil {
+		log.Printf("failed to listen: %v\n", err)
+		return
+	}
+	defer func() {
+		if cerr := lis.Close(); cerr != nil {
+			log.Printf("failed to close listener: %v\n", cerr)
+		}
+	}()
+
+	s := grpc.NewServer()
+	service := &paymentService{}
+
+	paymentV1.RegisterPaymentServiceServer(s, service)
+
+	// рефлексия для отладки
+	reflection.Register(s)
+
+	go func() {
+		log.Printf("Starting gRPC server at port %d", grpcPort)
+		err = s.Serve(lis)
+		if err != nil {
+			log.Printf("failed to serve: %v\n", err)
+			return
+		}
+	}()
+
+	// Graceful shutdown
+	quit := make(chan os.Signal, 1)
+	signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
+	<-quit
+
+	log.Println("🛑 Shutting down gRPC server...")
+	s.GracefulStop()
+	log.Println("✅ Server stopped")
+}
+
+type paymentService struct {
+	paymentV1.UnimplementedPaymentServiceServer
+}
+
+func (s *paymentService) PayOrder(context.Context, *paymentV1.PayOrderRequest) (*paymentV1.PayOrderResponse, error) {
+	transactionUUID := uuid.NewString()
+	log.Printf("Оплата прошла успешно, transaction_uuid: %s", transactionUUID)
+
+	return &paymentV1.PayOrderResponse{
+		TransactionUuid: transactionUUID,
+	}, nil
+}
